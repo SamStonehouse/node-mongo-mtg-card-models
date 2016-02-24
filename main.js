ab78cd4e379dfdcbbcb1c426037a2da7624a3fe3
Sam Stonehouse <Sam Stonehouse>
2016-02-24 20:21:37 +0000

    Added organisation, fixed imports

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var Card = require('card');
-var SetlessCard = require('setless-card');
-var Set = require('set');
+var Card = require('./source/card');
+var SetlessCard = require('./source/setless-card');
+var Set = require('./source/set');
 
 mdoule.exports = {
 	Card: Card,
